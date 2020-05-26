45b5c8b32d5b2ef377470b828071b5786ced8d43
Aziz Akbar <aziz.gen1@gmail.com>
2020-05-27 06:35:01 +0700

    tugas 2 js
    untuk Niomic

---
+++ Tugas2.js
@@ -0,0 +1,16 @@
+var number = 50;
+var string = 'Belajar JS';
+var boolean = true;
+
+if (number > 55)
+  number = false;
+else
+  number = true;
+
+var js = 'tugas2';
+js = null;
+var und;
+
+var array = ['merah', 'hijau', 'kuning', 'biru', 'abu-abu']
+
+console.log(number, string, boolean, js, und, array[2]);
